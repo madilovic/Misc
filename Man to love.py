20e0ad58c29208b1f850143b83cd78b1755c0c18
Muhamed Adilovic <muhammedadilovic@gmail.com>
2022-07-26 22:04:51 +0200

    Update Man to love.py
    Added option for alphabetic numbering since mp3 player in my car doesn't order numerals correctly

--- Man to love.py
+++ Man to love.py
@@ -1,11 +1,35 @@
 import os
 import json
 
+### If you want alphabetic numbering use this code ###
+
+first = ['a','b','c','d','e','f','g','h','i','j']
+second = ['a','b','c','d','e','f','g','h','i','j']
+combined = []
+
+for letter1 in first:
+    for letter2 in second:
+        combined.append(letter1+letter2)
+
 with open('Man to love.json', encoding="utf-8") as json_file:
     data = json.load(json_file)
     for chapter in data:
         for audio in data[chapter]:
-            os.rename(audio["FileName"]+'.mp3',str(audio["columnIndex"])+'_'+audio["TrackName"]+'.mp3')
+            index = audio["columnIndex"]
+            os.rename(audio["FileName"]+'.mp3',combined[index]+' '+audio["TrackName"]+'.mp3')
             # print(audio["FileName"])
             # print(audio["TrackName"])
             # print(audio["columnIndex"])
+            
+            
+### If you want numeric numbering use this code ###
+'''
+with open('Man to love.json', encoding="utf-8") as json_file:
+    data = json.load(json_file)
+    for chapter in data:
+        for audio in data[chapter]:
+            os.rename(audio["FileName"]+'.mp3',str(audio["columnIndex"])+' '+audio["TrackName"]+'.mp3')
+            # print(audio["FileName"])
+            # print(audio["TrackName"])
+            # print(audio["columnIndex"])
+'''
